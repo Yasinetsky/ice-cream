c34f63579b124281459ec977cc39aa3e7cdcfdc6
Sergey Shmatok <trycifer@gmail.com>
2022-12-18 16:22:54 +0200

    Fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-document.write('<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>');
-document.write('<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>');
 import './js/mobile-menu';
 import './js/slider';
 import './js/about-modal';
