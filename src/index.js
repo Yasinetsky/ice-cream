628de8a44765d657f1f14079274e268e2a595413
Sergey Shmatok <trycifer@gmail.com>
2022-12-18 15:51:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './js/mobile-menu';
+import './js/slider';
 import './js/about-modal';
 import './js/buynow-modal';
 import './js/franshise-modal';
 import './js/location-modal';
 import './js/readmore-modal';
-import './js/slider';
+
