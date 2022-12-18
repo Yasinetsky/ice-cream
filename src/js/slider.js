67993323de6bbc46941519e86d727bb5b24d8b93
Sergey Shmatok <trycifer@gmail.com>
2022-12-18 16:01:14 +0200

    Update slider.js

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,17 +1,4 @@
 $('.slideshow').slick({arrows: false, // не показывать стрелки
 dots: true, // показывать точки индикаторы
 dotsClass: 'slick-dots', // название класса для точек
-responsive: [{
-    breakpoint: 1025, // максимальная ширина экрана
-    settings: {
-        slidesToShow: 2, // показывать по 2 слайда
-        slidesToScroll: 2, // скроллить по 2 слайда
-    }
-}, {
-    breakpoint: 688,
-    settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    }
-}]
 });
