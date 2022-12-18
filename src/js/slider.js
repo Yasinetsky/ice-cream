7db5001766768c009d0f07e4469698a641935bf6
AntonVlasenkoo <antonvlasenko2005@gmail.com>
2022-12-18 19:29:46 +0200

    Додає слайдер та маржени

--- src/js/slider.js
+++ src/js/slider.js
@@ -4,3 +4,12 @@ $('.slideshow').slick({
 dots: true, // показувати точки індикатори
 dotsClass: 'slick-dots', // назва класу для точок
 });
+
+$('.gallery__slider').slick({
+    touchMove: true,
+autoplay: true,
+  autoplaySpeed: 2000,
+    arrows: false, // не показувати стрілки
+dots: false, // показувати точки індикатори
+dotsClass: 'slick-dots', // назва класу для точок
+});
